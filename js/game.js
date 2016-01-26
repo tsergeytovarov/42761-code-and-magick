08f4075f6d34a534c8c89c98c5201a6975f10c97
Сергей Попов <tovarov.piter@gmail.com>
2016-01-26 21:44:20 +0300

    Да так работает

--- js/game.js
+++ js/game.js
@@ -482,8 +482,8 @@
 
       for (i = 0; i < messageArray.length; i++) {
         var string = messageArray[i];
-        if (string[0] == ' ') {
-          if (string[string.length] == ' ') {
+        if (string[0] === ' ') {
+          if (string[string.length] === ' ') {
             string = string.substring(1, string.length - 1);
           } else {
             string = string.substring(1, string.length);
