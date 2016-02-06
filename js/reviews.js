d4242d99b2a7373e6027aaf43bc6eb8839c8872f
Сергей Попов <tovarov.piter@gmail.com>
2016-02-06 21:55:45 +0300

    Добавил загрузку по запросу и завернул некоторые функции

--- js/reviews.js
+++ js/reviews.js
@@ -5,15 +5,12 @@
   var reviewsFilter = document.querySelector('.reviews-filter');
   reviewsFilter.classList.add('invisible');
 
+  var reviewsWrapper = document.querySelector('.reviews');
   var reviewsContainer = document.querySelector('.reviews-list');
 
+  var loadedReviews;
 
-  reviews.forEach(function(item) {
-    var review = getReviewTemplate(item);
-    reviewsContainer.appendChild(review);
-  });
-
-  reviewsFilter.classList.remove('invisible');
+  getReviews();
 
   /**
    * Функция превращающая данные в шаблон
@@ -74,5 +71,53 @@
     } else {
       return reviewTemplate.children[0].cloneNode(true);
     }
+  }
+
+  /**
+   * Функция рисующая отзывы из данных
+   * @param {Array.Array} data
+   * @return {Element}
+   */
+  function drawReviews(data) {
+    data.forEach(function(item) {
+      var review = getReviewTemplate(item);
+      reviewsContainer.appendChild(review);
+    });
+  }
+
+  /**
+   * Функция забирающая данные через ajax с сервера
+   */
+  function getReviews() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '//o0.github.io/assets/json/reviews.json');
+    xhr.timeout = 10000;
+
+    // событие по началу загрузки
+    xhr.onloadstart = function() {
+      reviewsWrapper.classList.add('reviews-list-loading');
+    };
+
+    // событие если ошибка
+    xhr.onerror = function() {
+      picturesContainer.classList.add('reviews-load-failure');
+    };
+
+    // событие по тайауту
+    xhr.ontimeout = function() {
+      picturesContainer.classList.add('reviews-load-failure');
+    };
+
+    // событие по загрузке
+    xhr.onload = function(evt) {
+      reviewsWrapper.classList.remove('reviews-list-loading');
+      reviewsFilter.classList.remove('invisible');
+      var loadedData = evt.target.response;
+      loadedReviews = JSON.parse(loadedData);
+
+      drawReviews(loadedReviews);
+    };
+
+    xhr.send();
   }
 })();
