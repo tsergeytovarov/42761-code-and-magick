59f9c992a1112e2ef0df650c1d896c4833fd6db1
Сергей Попов <tovarov.piter@gmail.com>
2016-02-06 13:59:27 +0300

    Не то зацепил

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,6 @@
 /* global reviews: true */
 'use strict';
+
 (function() {
   var reviewsFilter = document.querySelector('.reviews-filter');
   reviewsFilter.classList.add('invisible');
@@ -11,4 +12,69 @@
     reviewsContainer.appendChild(review);
   });
 
+  reviewsFilter.classList.remove('invisible');
 
+  /**
+   * Функция превращающая данные в шаблон
+   * @param {Object} data
+   * @return {Element}
+   */
+  function getReviewTemplate(data) {
+    var reviewTemplate = document.querySelector('#review-template');
+
+    /* for IE */
+    if ('content' in reviewTemplate) {
+      var reviewElement = reviewTemplate.content.children[0].cloneNode(true);
+    } else {
+      var reviewElement = reviewTemplate.children[0].cloneNode(true);
+    }
+
+    var reviewText = reviewElement.querySelector('.review-text');
+
+    var reviewRatiogOld = reviewElement.querySelector('.review-rating');
+    reviewElement.removeChild(reviewRatiogOld);
+
+    var reviewRatingNew;
+
+    var ratingContainer = document.createElement('div');
+    ratingContainer.classList.add('ratingContainer');
+    reviewElement.insertBefore(ratingContainer, reviewText);
+    var rating = data.rating;
+    console.log(rating);
+
+    for (var i = 0; i < rating; i++) {
+      var reviewRatingNew = document.createElement('span');
+      reviewRatingNew.classList.add('review-rating');
+      ratingContainer.appendChild(reviewRatingNew);
+    }
+
+    reviewText.textContent = data.description;
+
+    var reviewImage = new Image(124, 124);
+    reviewImage.src = data.author.picture;
+    reviewImage.alt = data.author.name;
+
+    var reviewImageLoadTimeout;
+
+    reviewImage.onload = function() {
+      clearTimeout(reviewImageLoadTimeout);
+      var templateImage = reviewElement.querySelector('.review-author');
+      var imageClass = templateImage.classList;
+      reviewImage.classList.add(imageClass);
+      reviewElement.replaceChild(reviewImage, templateImage);
+    };
+
+    reviewImage.onerror = function() {
+      reviewElement.classList.add('review-load-failure');
+    };
+
+    var REVIEW_IMAGE_TIMEOUT = 5000;
+
+    reviewImageLoadTimeout = setTimeout(function() {
+      reviewImage.src = '';
+      reviewElement.classList.add('review-load-failure');
+    }, REVIEW_IMAGE_TIMEOUT);
+
+    return reviewElement;
+  }
+})();
